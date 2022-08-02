983e711378e9a5a951a78f91d61b96a27de9cc80
miguelcr85 <mcramirez21.12.85@gmail.com>
2022-08-01 21:34:02 -0400

    1 version tcp

--- assets/js/_core.js
+++ assets/js/_core.js
@@ -28,22 +28,36 @@ function scrollSpy() {
 
 
 d.addEventListener('DOMContentLoaded', (e) => {
-  var elms = document.getElementsByClassName('splide');
 
-  for (var i = 0; i < elms.length; i++) {
-    new Splide(elms[i], {
-      type: 'loop',
-      autoplay: true,
-      interval: 3000,
-    }).mount();
-  }
+  new Splide('#slide1',{
+    type: 'loop',
+    autoplay: true,
+    interval: 3000,
+  }).mount();
+
+  new Splide('#slide2',{
+    type: 'loop',
+    autoplay: true,
+    interval: 3000,
+  }).mount();
+
+  new Splide('#slide3',{
+    type: 'loop',
+    autoplay: true,
+    interval: 3000,
+    perPage: 3,
+    rewind : true,
+  }).mount();
+
+
+  
   scrollSpy()
 });
 d.addEventListener('submit', (e) => {
   const $myform = d.getElementById('scf-form-id-contactForm'),
     data = new FormData(e.target),
     $formSpace = d.getElementById('formSpace'),
-    text = `${data.name}<p class="text-success text-capitalize fs-2">your data has been sent successfully </p>`;
+    text = `<p class="text-success text-capitalize fs-2">your data has been sent successfully </p>`;
   e.preventDefault();
   axios({
     method: 'POST',
@@ -53,7 +67,7 @@ d.addEventListener('submit', (e) => {
     .then(function (response) {
       $myform.reset();
       console.log(data);
-      //console.log(response);
+      console.log(response);
       //$myform.classList.add('d-none');
       //$formSpace.innerHTML = text
     })
